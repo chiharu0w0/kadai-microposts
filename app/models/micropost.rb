544f03f45ad786a820ffdf3d1cb89f8c0c907b73
kussy <kussy.sample@gmail.com>
2019-07-30 04:32:36 +0000

    お気に入り後の削除機能修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :favorites, foreign_key: 'micropost_id'
+  has_many :favorites, foreign_key: 'micropost_id', dependent: :destroy
   has_many :users, through: :favorites
   validates :content, presence: true, length: { maximum: 255 }
 end
